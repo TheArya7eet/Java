6452d11b9fd3531ad4c10c4eb35aefc511129f24
Aryajeet Ghosh <aryajeetghosh1@gmail.com>
2023-10-05 00:07:12 +0530

    file updated: KadanesAlgorithm.java

--- Problem-Solving/Arrays/src/KadanesAlgorithm.java
+++ Problem-Solving/Arrays/src/KadanesAlgorithm.java
@@ -4,11 +4,15 @@ public class KadanesAlgorithm{
         int maxSum = Integer.MIN_VALUE;
 
         for(int i = 0; i < n; i++){
+          // Add the current element to currSum
           currSum += arr[i];
 
+          //  Store the maximum of maxSum and currSum
           maxSum = Math.max(maxSum, currSum);
           
+          //  If the current sum is < 0
           if(currSum < 0){
+            // Update the value of currSum back to 0
             currSum = 0;
           }
         }
