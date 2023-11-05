e0ca3eabd9059ddc9d3add9d6d7ef988db5a5459
Aryajeet Ghosh <aryajeetghosh1@gmail.com>
2023-11-05 21:41:40 +0530

    files updated

--- Problem-Solving/Strings/src/ReverseString.java
+++ Problem-Solving/Strings/src/ReverseString.java
@@ -4,7 +4,9 @@ public class ReverseString {
     private static void reverseString(char[] str){
         int n = str.length;
 
+        // Run a loop i ranging from 0 to (n/2 - 1)
         for(int i = 0; i < n / 2; i++){
+            // Reverse the string by swapping the characters
             char temp = str[i];
             str[i] = str[n - 1 - i];
             str[n - 1 - i] = temp; 
